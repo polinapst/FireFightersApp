50a8107b056b17be792001cc5a76105c8d5cf093
polinatuzankina <polinatuzankina@gmail.com>
2022-05-22 20:47:19 +0600

    Add project files.

---
+++ FireFightersApp/Models/Call.cs
@@ -0,0 +1,11 @@
+﻿namespace FireFightersApp.Models
+{
+    public class Call
+    {
+        public int CallId { get; set; }
+        public string CallerId { get; set; }
+        public string Address { get; set; }
+        public DateTime CallDatetime { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ FireFightersApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FireFightersApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
